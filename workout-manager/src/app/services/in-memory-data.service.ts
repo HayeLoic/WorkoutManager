1e1b0d98002bd1dae66889cd2f95e4232fb7445c
Loïc HAYE <haye.loic@gmail.com>
2020-03-14 10:08:58 +0100

    add in memory data service

---
+++ workout-manager/src/app/services/in-memory-data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Workout } from '../models/workout';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDataService {
+  createDb() {
+    const workouts: Workout[] = [
+      { id: 1, name: 'Aphrodite Version Annie' },
+      { id: 2, name: 'Workout 2' },
+      { id: 3, name: 'Workout 3' },
+      { id: 4, name: 'Workout 4' },
+      { id: 5, name: 'Workout 5' }
+    ];
+    return workouts;
+  }
+
+  genId(workouts: Workout[]): number {
+    return workouts.length > 0 ? Math.max(...workouts.map(workout => workout.id)) + 1 : 1;
+  }
+}
--- workout-manager/src/app/services/workout.service.ts
+++ workout-manager/src/app/services/workout.service.ts
@@ -2,24 +2,42 @@ import { Injectable } from '@angular/core';
 import { MessageService } from './message.service';
 import { Observable, of } from 'rxjs';
 import { Workout } from '../models/workout';
-import { WORKOUTS } from '../mocks/mock-workouts';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkoutService {
+  private workoutsUrl = 'api/workouts';
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
   getWorkouts(): Observable<Workout[]> {
-    // TODO: send the message _after_ fetching the Workouts
     this.messageService.add('WorkoutService: fetched Workouts');
-    return of(WORKOUTS);
+    return this.http.get<Workout[]>(this.workoutsUrl).pipe(
+      tap(_ => this.log('fetched workouts')),
+      catchError(this.handleError<Workout[]>('getWorkouts', []))
+    );
   }
 
   getWorkout(id: number): Observable<Workout> {
-    // TODO: send the message _after_ fetching the workout
-    this.messageService.add(`WorkoutService: fetched workout id=${id}`);
-    return of(WORKOUTS.find(workout => workout.id === id));
+    const url = `${this.workoutsUrl}/${id}`;
+    return this.http.get<Workout>(url).pipe(
+      tap(_ => this.log(`fetched workout id=${id}`)),
+      catchError(this.handleError<Workout>(`getWorkout id=${id}`))
+    );
+  }
+
+  private log(message: string) {
+    this.messageService.add(`WorkoutService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 }
