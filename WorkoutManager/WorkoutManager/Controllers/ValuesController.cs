42a1c83ec381bcfde3139670163ae6a6e9cf7ecc
Loïc HAYE <haye.loic@gmail.com>
2020-03-17 23:43:19 +0100

    poc mariaDb connection

--- WorkoutManager/WorkoutManager/Controllers/ValuesController.cs
+++ WorkoutManager/WorkoutManager/Controllers/ValuesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 
 namespace WorkoutManager.Controllers
 {
@@ -14,6 +16,19 @@ namespace WorkoutManager.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            string m_strMySQLConnectionString = "server=localhost;userid=WorkoutManagerAdmin;database=WorkoutManager;password=Workout2020";
+
+            try
+            {
+                string strQueryGetValue = "select Name from Workout where id = 1";
+                string strValue = GetValueFromDBUsing(strQueryGetValue, m_strMySQLConnectionString);
+                return new string[] { strValue };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
             return new string[] { "value1", "value2" };
         }
 
@@ -40,6 +55,61 @@ namespace WorkoutManager.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+
+        private string GetValueFromDBUsing(string strQuery, string m_strMySQLConnectionString)
+        {
+            string strData = "";
+
+            try
+            {
+                if (string.IsNullOrEmpty(strQuery) == true)
+                    return string.Empty;
+
+                using (var mysqlconnection = new MySqlConnection(m_strMySQLConnectionString))
+                {
+                    mysqlconnection.Open();
+                    using (MySqlCommand cmd = mysqlconnection.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.CommandTimeout = 300;
+                        cmd.CommandText = strQuery;
+
+                        object objValue = cmd.ExecuteScalar();
+                        if (objValue == null)
+                        {
+                            cmd.Dispose();
+                            return string.Empty;
+                        }
+                        else
+                        {
+                            strData = (string)cmd.ExecuteScalar();
+                            cmd.Dispose();
+                        }
+
+                        mysqlconnection.Close();
+
+                        if (strData == null)
+                            return string.Empty;
+                        else
+                            return strData;
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex);
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return string.Empty;
+            }
+            finally
+            {
+
+            }
         }
     }
 }
